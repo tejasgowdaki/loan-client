6bc8467023bd5eb2fe27dc60b6959b447df5b13e
Tejas Gowda <tejas@syook.com>
2020-07-25 22:31:26 +0530

    fixed height for mobile

--- src/features/layout/mobile.js
+++ src/features/layout/mobile.js
@@ -1,17 +1,9 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import {
-  Container,
-  Icon,
-  Menu,
-  Responsive,
-  Segment,
-  Sidebar,
-  
-} from "semantic-ui-react";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Container, Icon, Menu, Responsive, Segment, Sidebar } from 'semantic-ui-react';
 
 const getWidth = () => {
-  const isSSR = typeof window === "undefined";
+  const isSSR = typeof window === 'undefined';
 
   return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
 };
@@ -32,59 +24,44 @@ class Mobile extends Component {
         as={Sidebar.Pushable}
         getWidth={getWidth}
         maxWidth={Responsive.onlyMobile.maxWidth}
+        style={{ height: '100vh' }}
       >
-        <Sidebar
-          as={Menu}
-          animation="push"
-          inverted
-          onHide={this.handleSidebarHide}
-          vertical
-          visible={sidebarOpened}
-        >
-          <Menu.Item
-            as="a"
-            active={this.props.pathname === "/"}
-            onClick={() => this.props.onClickNavigate("/")}
-          >
+        <Sidebar as={Menu} animation="push" inverted onHide={this.handleSidebarHide} vertical visible={sidebarOpened}>
+          <Menu.Item as="a" active={this.props.pathname === '/'} onClick={() => this.props.onClickNavigate('/')}>
             Home
           </Menu.Item>
 
           <Menu.Item
             as="a"
-            active={this.props.pathname === "/members"}
-            onClick={() => this.props.onClickNavigate("/members")}
+            active={this.props.pathname === '/members'}
+            onClick={() => this.props.onClickNavigate('/members')}
           >
             Members
           </Menu.Item>
 
           <Menu.Item
             as="a"
-            active={this.props.pathname === "/loans"}
-            onClick={() => this.props.onClickNavigate("/loans")}
+            active={this.props.pathname === '/loans'}
+            onClick={() => this.props.onClickNavigate('/loans')}
           >
             Loans
           </Menu.Item>
 
           <Menu.Item
             as="a"
-            active={this.props.pathname === "/savings"}
-            onClick={() => this.props.onClickNavigate("/savings")}
+            active={this.props.pathname === '/savings'}
+            onClick={() => this.props.onClickNavigate('/savings')}
           >
             Savings
           </Menu.Item>
 
           <Menu.Item as="a">Log in</Menu.Item>
-          
+
           <Menu.Item as="a">Sign Up</Menu.Item>
         </Sidebar>
 
         <Sidebar.Pusher dimmed={sidebarOpened}>
-          <Segment
-            inverted
-            textAlign="center"
-            style={{ minHeight: 50, padding: "1em 0em" }}
-            vertical
-          >
+          <Segment inverted textAlign="center" style={{ minHeight: 50, padding: '1em 0em' }} vertical>
             <Container>
               <Menu inverted pointing secondary size="large">
                 <Menu.Item onClick={this.handleToggle}>
